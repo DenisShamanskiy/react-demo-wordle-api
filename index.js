7a91491ab27d095c5e08fd3b74127b2fa2cea5bd
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2022-10-10 14:49:24 +0300

    Инициализация проекта

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const port = 3002;
+
+app.use("/", (req, res) => {
+  res.json({ message: "Hello From Express App" });
+});
+
+app.listen(9000, () => {
+  console.log(`Starting Server on Port ${port}`);
+});
