63b8ed4b63f4d9465a4250725d4461e094597f62
DenisShamanskiy <75094596+DenisShamanskiy@users.noreply.github.com>
2022-12-08 22:09:27 +0300

    Исправлена ошибка CORS

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const router = require("./router/index.js");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const PORT = process.env.PORT || 3002;
-const whitelist = process.env.CLIENT_URL_LIST.split(",").map((origin) =>
-  origin.trim()
-);
-const corsOptions = {
-  credentials: true,
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+
 app.use("/api", router);
 app.use(errorMiddleware);
 
